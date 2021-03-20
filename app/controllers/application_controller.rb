72ef5529422b16916a3afed386a10fc480862c82
Kiana Long <kianajade42.kl@gmail.com>
2021-03-20 16:45:18 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,6 +33,18 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
+    post "/login" do
+ 	user=User.find_by(:username =>
+	params[:username])
+	if user&&
+		user.authenticate(params[:password])
+    session[:user_id] = user.id
+		redirect "/success"
+	else
+		redirect "/failure"
+	end
+ end
+
   end
 
   get "/failure" do
